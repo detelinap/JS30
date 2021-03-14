45652c106503f7fcd436621209acaebeebb2e116
Detelina <palova.detelina@gmail.com>
2021-03-14 22:46:48 +0200

    Tally String Times with Reduce

--- day-eighteen/script.js
+++ day-eighteen/script.js
@@ -1,14 +1,13 @@
 const timeNodes = Array.from(document.querySelectorAll("[data-time]"));
 
 const seconds = timeNodes
-    .map((node) => node.dataSet.time)
-    .map((timeCode) => {
+    .map(node => node.dataset.time)
+    .map(timeCode => {
         const [mins, secs] = timeCode.split(":").map(parseFloat);
         return mins * 60 + secs;
     })
-    .reduce((total, seconds) => {
-        total + seconds;
-    });
+    .reduce((total, videoSeconds) =>
+        total + videoSeconds);
 
 let secondsLeft = seconds;
 const hours = Math.floor(secondsLeft / 3600);
